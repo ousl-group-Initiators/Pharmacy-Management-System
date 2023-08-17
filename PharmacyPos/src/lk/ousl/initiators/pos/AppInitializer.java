5368d7a653782f23aa6bba8a5bce758568a890cd
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-17 10:57:52 +0530

    update the AppInitializer.java file

--- PharmacyPos/src/lk/ousl/initiators/pos/AppInitializer.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/AppInitializer.java
@@ -1,9 +1,12 @@
 package lk.ousl.initiators.pos;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -16,7 +19,16 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene((FXMLLoader.load(Objects.requireNonNull(getClass().getResource("./view/Dashboard.fxml"))))));
+        primaryStage.setScene(new Scene((FXMLLoader.load(Objects.requireNonNull(getClass().getResource("./view/Login.fxml"))))));
+        primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(2500));
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
+        fadeTransition.play();
+
     }
 }
