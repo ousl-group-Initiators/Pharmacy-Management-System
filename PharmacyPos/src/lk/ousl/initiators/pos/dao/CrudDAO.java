d02a14b0bb7cb948cfb766f84fe03cf9a9e833d3
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-09-22 09:32:29 +0530

    create a common methods

---
+++ PharmacyPos/src/lk/ousl/initiators/pos/dao/CrudDAO.java
@@ -0,0 +1,16 @@
+package lk.ousl.initiators.pos.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CrudDAO <T,ID> extends SuperDAO {
+    boolean save(T t) throws SQLException, ClassNotFoundException;
+
+    boolean delete(ID id) throws SQLException, ClassNotFoundException;
+
+    boolean update(T t) throws SQLException, ClassNotFoundException;
+
+    T search(ID id) throws SQLException, ClassNotFoundException;
+
+    ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+}
--- PharmacyPos/src/lk/ousl/initiators/pos/dao/CrudDao.java
+++
@@ -1,5 +0,0 @@
-package lk.ousl.initiators.pos.dao;
-
-public interface CrudDao extends SuperDao{
-
-}
