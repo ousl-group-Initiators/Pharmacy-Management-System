0f8a50dc35c6bb3ed75a4e7ec4b9551459e66f41
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-09-22 09:36:06 +0530

    refactor EmployeeDAO interface name

---
+++ PharmacyPos/src/lk/ousl/initiators/pos/dao/custom/EmployeeDAO.java
@@ -0,0 +1,7 @@
+package lk.ousl.initiators.pos.dao.custom;
+
+import lk.ousl.initiators.pos.dao.CrudDAO;
+import lk.ousl.initiators.pos.entity.Employee;
+
+public interface EmployeeDAO extends CrudDAO<Employee,String> {
+}
