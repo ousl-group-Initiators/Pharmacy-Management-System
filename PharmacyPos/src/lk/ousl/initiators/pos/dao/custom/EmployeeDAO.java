fbb2abe339b8c6a1824e29ad98f1516ec0871e07
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-09-23 20:41:57 +0530

    add new method

--- PharmacyPos/src/lk/ousl/initiators/pos/dao/custom/EmployeeDAO.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/dao/custom/EmployeeDAO.java
@@ -3,5 +3,11 @@ package lk.ousl.initiators.pos.dao.custom;
 import lk.ousl.initiators.pos.dao.CrudDAO;
 import lk.ousl.initiators.pos.entity.Employee;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface EmployeeDAO extends CrudDAO<Employee,String> {
+
+    boolean ifEmployeeExist(String id) throws SQLException, ClassNotFoundException;
+    public ArrayList<Employee> searchEmployee (String searchText) throws SQLException, ClassNotFoundException;
 }
