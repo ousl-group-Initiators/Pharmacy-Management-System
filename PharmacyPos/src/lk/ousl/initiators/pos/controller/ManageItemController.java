e279e097271cb3c190fd808e7b0eb3b0166b68e6
Nimanthika03 <nimanthikamalithi03@gmail.com>
2023-11-10 00:54:09 +0530

    Update manage item

--- PharmacyPos/src/lk/ousl/initiators/pos/controller/ManageItemController.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/controller/ManageItemController.java
@@ -1,4 +1,12 @@
 package lk.ousl.initiators.pos.controller;
 
+import javafx.event.ActionEvent;
+
 public class ManageItemController {
+    public void btnNewEmployeeOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnSaveEmployeeOnAction(ActionEvent actionEvent) {
+    }
 }
