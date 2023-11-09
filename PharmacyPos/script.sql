2268b64e34a250e99822639585e7ed1b8d536a1b
DESKTOP-9J4J406\IRESHA <techflows713@gmail.com>
2023-11-09 08:41:57 +0530

    create drugs table

--- PharmacyPos/script.sql
+++ PharmacyPos/script.sql
@@ -11,19 +11,40 @@ USE Pharmacy;
 
 -- # --------------------------------------
 -- # create Employee table
-DROP DATABASE IF EXISTS Employee;
-CREATE TABLE IF NOT EXISTS Employee(
-    nic_number VARCHAR(50) NOT NULL,
-    first_name VARCHAR(150) NOT NULL,
-    last_name VARCHAR(150) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    age INT NOT NULL,
-    telephone_number INT NOT NULL,
-    Address VARCHAR(350) NOT NULL,
-    job_role VARCHAR(200) NOT NULL,
-    description VARCHAR(350) NOT NULL,
-    CONSTRAINT PRIMARY KEY (nic_number)
+DROP TABLE IF EXISTS Employee;
+CREATE TABLE IF NOT EXISTS Employee
+(
+    emp_id           VARCHAR(50)  NOT NULL,
+    first_name       VARCHAR(150) NOT NULL,
+    last_name        VARCHAR(150) NOT NULL,
+    date_of_birth    DATE         NOT NULL,
+    age              INT          NOT NULL,
+    telephone_number INT          NOT NULL,
+    address          VARCHAR(350) NOT NULL,
+    job_role         VARCHAR(200) NOT NULL,
+    description      VARCHAR(350) NOT NULL,
+    CONSTRAINT PRIMARY KEY (emp_id)
 );
 
 -- Employee Address Table
 
+select *
+from employee;
+
+-- # --------------------------------------
+-- # create Drugs table
+DROP TABLE IF EXISTS Drugs;
+CREATE TABLE Drugs
+(
+    drug_id         VARCHAR(50)     NOT NULL,
+    drug_name       VARCHAR(100)    NOT NULL,
+    batch_number    VARCHAR(50)     NOT NULL,
+    drug_quantity   INT             NOT NULL,
+    unit_price      DECIMAL(10, 2)  NOT NULL,
+    unit_discount   DECIMAL(10, 2)  NOT NULL,
+    MFD             DATE            NOT NULL,
+    EXD             DATE            NOT NULL,
+    Supply_id       VARCHAR(50)     NOT NULL,
+    description     VARCHAR(350)    NOT NULL,
+    CONSTRAINT PRIMARY KEY (drug_id)
+);
