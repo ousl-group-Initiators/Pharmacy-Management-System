fef77140d59b4c6266d8273ff66de07457d862fe
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-11-09 07:54:27 +0530

    update Inventory.fxml and InventoryController files

--- PharmacyPos/src/lk/ousl/initiators/pos/controller/InventoryController.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/controller/InventoryController.java
@@ -1,7 +1,18 @@
 package lk.ousl.initiators.pos.controller;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class InventoryController {
     public AnchorPane InventoryContext;
+    public TableView tblInventory;
+    public TableColumn colName;
+    public TableColumn colWeight;
+    public TableColumn colCategory;
+    public TableColumn colPrice;
+    public TableColumn colStock;
+    public TableColumn colStatus;
+    public TextField txtSearch;
 }
