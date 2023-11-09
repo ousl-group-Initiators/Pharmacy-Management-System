a0ff7493b71ad057a09d0e3aa11d0ec903e510de
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-11-09 23:42:11 +0530

    update Registration Form controller

--- PharmacyPos/src/lk/ousl/initiators/pos/controller/RegistrationFormController.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/controller/RegistrationFormController.java
@@ -1,7 +1,18 @@
 package lk.ousl.initiators.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class RegistrationFormController {
     public AnchorPane RegistrationFormContext;
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtPassword;
+    public JFXTextField txtConfirmPassword;
+    public JFXPasswordField txtJobRole;
+
+    public void RegisterOnAction(ActionEvent actionEvent) {
+
+    }
 }
