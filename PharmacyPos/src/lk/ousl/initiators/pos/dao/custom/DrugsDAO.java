89c157fb873e6bb867fd9c49ad558930410dd1ac
DESKTOP-9J4J406\IRESHA <techflows713@gmail.com>
2023-11-11 12:47:51 +0530

    Update DrugsDAO interface

--- PharmacyPos/src/lk/ousl/initiators/pos/dao/custom/DrugsDAO.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/dao/custom/DrugsDAO.java
@@ -1,4 +1,13 @@
 package lk.ousl.initiators.pos.dao.custom;
 
-public interface DrugsDAO {
+import lk.ousl.initiators.pos.dao.CrudDAO;
+import lk.ousl.initiators.pos.entity.Drugs;
+import lk.ousl.initiators.pos.entity.Employee;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface DrugsDAO extends CrudDAO<Drugs,String> {
+    boolean ifDrugsExist(String id) throws SQLException, ClassNotFoundException;
+    public ArrayList<Drugs> searchDrugs (String searchText) throws SQLException, ClassNotFoundException;
 }
