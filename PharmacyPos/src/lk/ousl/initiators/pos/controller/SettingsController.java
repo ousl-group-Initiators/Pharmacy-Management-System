4d642812aa082819fd8042d75c8d2ab88d7d5d7d
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-11-22 09:19:20 +0530

    create Settings controller class

---
+++ PharmacyPos/src/lk/ousl/initiators/pos/controller/SettingsController.java
@@ -0,0 +1,14 @@
+package lk.ousl.initiators.pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class SettingsController {
+    public AnchorPane SettingsContext;
+
+    public void btnRegisterUsersOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnViewContactDetailsOnAction(ActionEvent actionEvent) {
+    }
+}
