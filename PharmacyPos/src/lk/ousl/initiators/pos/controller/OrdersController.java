8cf476a837616b026ec506f8a9ac91255cee2f26
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-11-22 09:20:38 +0530

    create Order Controller Class

--- PharmacyPos/src/lk/ousl/initiators/pos/controller/OrdersController.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/controller/OrdersController.java
@@ -1,7 +1,18 @@
 package lk.ousl.initiators.pos.controller;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class OrdersController {
     public AnchorPane OrdersContext;
+    public TextField txtSearch;
+    public TableView tblOrders;
+    public TableColumn colInvoiceNumber;
+    public TableColumn colCashierName;
+    public TableColumn colDate;
+    public TableColumn colTime;
+    public TableColumn colTotal;
+    public TableColumn colOption;
 }
