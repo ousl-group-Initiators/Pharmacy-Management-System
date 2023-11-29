85dee0d292591eac1528d9444a327ae68da63567
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-11-29 23:28:35 +0530

    Create Order Details.fxml file and OrderDetailsController Class

---
+++ PharmacyPos/src/lk/ousl/initiators/pos/controller/OrderDetailsController.java
@@ -0,0 +1,16 @@
+package lk.ousl.initiators.pos.controller;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class OrderDetailsController {
+    public AnchorPane OrderDetailsContext;
+    public TableView tblOrderDetails;
+    public TableColumn colItemCode;
+    public TableColumn colDescription;
+    public TableColumn colUnitPrice;
+    public TableColumn colQty;
+    public TableColumn colDiscount;
+    public TableColumn colTotal;
+}
