ebd40e9a861e22067f8c42863ce6d1d143ce532a
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-12-02 08:23:30 +0530

    update Billing Dashboard Content

--- PharmacyPos/src/lk/ousl/initiators/pos/controller/BillingDashboardController.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/controller/BillingDashboardController.java
@@ -1,9 +1,18 @@
 package lk.ousl.initiators.pos.controller;
 
 import com.jfoenix.controls.JFXComboBox;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BillingDashboardController {
     public Label lblDate;
@@ -16,6 +25,23 @@ public class BillingDashboardController {
     public Label lblInvoiceNo;
     public TableView tblBillingTable;
     public Label lblTotal;
+    public Label lblDateTime;
+
+    public void initialize() throws IOException {
+        setDateAndTime();
+    }
+
+    // create the local date and time
+    private void setDateAndTime() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, e -> {
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd   |   HH:mm:ss");
+                    lblDateTime.setText(LocalDateTime.now().format(formatter));
+                }),new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
 
     public void btnPlaceOrderOnAction(ActionEvent actionEvent) {
     }
