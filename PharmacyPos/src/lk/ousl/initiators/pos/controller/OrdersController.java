bd593fa8ffa30e6eea06cecb83f87f7086296686
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-12-06 19:08:18 +0530

    update OrdersController class

--- PharmacyPos/src/lk/ousl/initiators/pos/controller/OrdersController.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/controller/OrdersController.java
@@ -2,6 +2,7 @@ package lk.ousl.initiators.pos.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,6 +16,7 @@ import javafx.stage.Stage;
 import lk.ousl.initiators.pos.bo.BoFactory;
 import lk.ousl.initiators.pos.bo.custom.OrderBO;
 import lk.ousl.initiators.pos.db.DBConnection;
+import lk.ousl.initiators.pos.dto.EmployeeDTO;
 import lk.ousl.initiators.pos.dto.OrderDTO;
 
 import java.io.IOException;
@@ -23,6 +25,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class OrdersController {
@@ -35,10 +38,11 @@ public class OrdersController {
     public TableColumn<OrderDTO, Time> colTime;
     public TableColumn<OrderDTO, Double> colTotal;
     public TableColumn<OrderDTO, Button> colOption;
+    private String searchText = "";
 
     private final OrderBO orderBO = (OrderBO) BoFactory.getBoFactory().getBo(BoFactory.BoTypes.ORDER);
 
-    public void initialize(){
+    public void initialize() {
         colInvoiceNumber.setCellValueFactory(new PropertyValueFactory<>("invoice_number"));
         colCashierName.setCellValueFactory(new PropertyValueFactory<>("cashier_name"));
         colDate.setCellValueFactory(new PropertyValueFactory<>("date"));
@@ -47,26 +51,35 @@ public class OrdersController {
         colOption.setCellValueFactory(new PropertyValueFactory<>("button"));
 
         loadOrders();
+
+        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+            searchText = newValue;
+            searchOrders(searchText);
+        });
+        searchOrders(searchText);
+
     }
 
-    private void loadOrders(){
-        try{
-            String sql = "SELECT * FROM `Order`";
-            PreparedStatement statement = DBConnection.getDbConnection().getConnection().prepareStatement(sql);
-            ResultSet set = statement.executeQuery();
+    private void searchOrders(String text){
+        String searchText = "%" + text + "%";
+        ObservableList<OrderDTO> orderDTOS = FXCollections.observableArrayList();
+        try {
+            // Assuming orderBO.getAllOrders() returns an ArrayList<OrderDTO>
+            ArrayList<OrderDTO> allOrders = orderBO.searchOrder(searchText);
 
-            ObservableList<OrderDTO> obsOderList = FXCollections.observableArrayList();
-            while (set.next()){
-
+            for (OrderDTO orders : allOrders) {
                 Button btn = new Button("View more");
+                OrderDTO orderDTO = new OrderDTO(
+                        orders.getInvoice_number(),
+                        orders.getCashier_name(),
+                        orders.getDate(),
+                        orders.getTime(),
+                        orders.getTotal(),
+                        btn
+                );
 
-                OrderDTO orderDTO = new OrderDTO(set.getString(1),
-                        set.getString(2),
-                        set.getDate(3),  //set.getString(2)
-                        set.getTime(4),
-                        set.getDouble(5),
-                        btn);
-                obsOderList.add(orderDTO);
+                // Add the current orderDTO to the observable list
+                orderDTOS.add(orderDTO);
 
                 btn.setOnAction(event -> {
                     try {
@@ -83,22 +96,53 @@ public class OrdersController {
                     }
                 });
             }
-            tblOrders.setItems(obsOderList);
-        }catch (SQLException e){
+        }  catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
+        // Set the items in tblOrders with the observable list
+        tblOrders.setItems(orderDTOS);
     }
 
-
-
-
-
-
-
-
-
-
-
-
+    private void loadOrders() {
+//        ObservableList<OrderDTO> orderDTOS = FXCollections.observableArrayList();
+//        try {
+//            // Assuming orderBO.getAllOrders() returns an ArrayList<OrderDTO>
+//            ArrayList<OrderDTO> allOrders = orderBO.getAllOrders();
+//
+//            for (OrderDTO orders : allOrders) {
+//                Button btn = new Button("View more");
+//                OrderDTO orderDTO = new OrderDTO(
+//                        orders.getInvoice_number(),
+//                        orders.getCashier_name(),
+//                        orders.getDate(),
+//                        orders.getTime(),
+//                        orders.getTotal(),
+//                        btn
+//                );
+//
+//                // Add the current orderDTO to the observable list
+//                orderDTOS.add(orderDTO);
+//
+//                btn.setOnAction(event -> {
+//                    try {
+//                        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/OrderDetails.fxml"));
+//                        Parent parent = loader.load();
+//                        OrderDetailsController controller = loader.getController();
+//                        controller.loadItemDetails(orderDTO.getInvoice_number());
+//                        Stage stage = new Stage();
+//                        stage.setScene(new Scene(parent));
+//                        stage.show();
+//
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                });
+//            }
+//        }  catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//        }
+//        // Set the items in tblOrders with the observable list
+//        tblOrders.setItems(orderDTOS);
+    }
 
 }
