2b020b45573f2af72201c792e456ec8926988365
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-12-06 19:04:42 +0530

    create new methods in Dashboard Controller Class

--- PharmacyPos/src/lk/ousl/initiators/pos/controller/DashboardController.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/controller/DashboardController.java
@@ -5,11 +5,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import lk.ousl.initiators.pos.bo.BoFactory;
+import lk.ousl.initiators.pos.bo.custom.DashboardBO;
+import lk.ousl.initiators.pos.bo.custom.OrderBO;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Objects;
 
 public class DashboardController {
@@ -17,6 +22,24 @@ public class DashboardController {
     public AnchorPane dashboardContext;
     public JFXButton btnMangeDrugs;
     public JFXButton btnManageItem;
+    public Label lblExpireDrugs;
+    public Label lblDrugOutOfStock;
+    public Label lblExpiredItems;
+    public Label lblItemOutOfStock;
+    public Label lblStockedDrugs;
+    public Label lblStockedItems;
+    public Label lblTotalEmployee;
+    public JFXButton btnManageEmployee;
+    public Label lblTotalSuppliers;
+    public JFXButton btnManageSupplier;
+
+    private final DashboardBO dashboardBO = (DashboardBO) BoFactory.getBoFactory().getBo(BoFactory.BoTypes.DASHBOARD);
+
+    public void initialize() {
+        calculateEmployeeCount();
+        calculateSuppliersCount();
+        calculateDrugsCount();
+    }
 
     // create View Billing Dashboard button OnAction method to load the billingDashboard ui
     public void viewBillingDashboardOnAction(ActionEvent actionEvent) throws IOException {
@@ -42,5 +65,68 @@ public class DashboardController {
         stage.centerOnScreen();
         stage.setResizable(false);
         stage.show();
+    }
+
+    public void manageDrugsOnAction02(ActionEvent actionEvent) throws IOException {
+        manageDrugsOnAction(actionEvent);
+    }
+
+    public void manageItemOnAction02(ActionEvent actionEvent) throws IOException {
+        manageItemOnAction(actionEvent);
+    }
+
+    public void manageDrugsOnAction03(ActionEvent actionEvent) throws IOException {
+        manageDrugsOnAction(actionEvent);
+    }
+
+    public void manageItemOnAction03(ActionEvent actionEvent) throws IOException {
+        manageItemOnAction(actionEvent);
+    }
+
+    public void ManageEmployeeOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/ManageEmployee.fxml"));
+        Parent parent = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(parent));
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void ManageSupplierOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/ManageSuppliers.fxml"));
+        Parent parent = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(parent));
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void calculateEmployeeCount() {
+        try {
+            int employeeCount = dashboardBO.getEmployeeCount();
+            lblTotalEmployee.setText(String.valueOf(employeeCount));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void calculateSuppliersCount() {
+        try {
+            int supplierCount = dashboardBO.getSuppliersCount();
+            lblTotalEmployee.setText(String.valueOf(supplierCount));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void calculateDrugsCount() {
+        try {
+            int drugsCount = dashboardBO.getDrugsCount();
+            lblStockedDrugs.setText(String.valueOf(drugsCount));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
