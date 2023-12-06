fbae62b21964eac95cd51abf45718c91c60a5017
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-12-06 19:07:57 +0530

    update OrderDetailsDTO, OrderDetailsDAO, OrderDetailsDAOImpl class

--- PharmacyPos/src/lk/ousl/initiators/pos/dao/custom/OrderDetailsDAO.java
+++ PharmacyPos/src/lk/ousl/initiators/pos/dao/custom/OrderDetailsDAO.java
@@ -3,5 +3,8 @@ package lk.ousl.initiators.pos.dao.custom;
 import lk.ousl.initiators.pos.dao.CrudDAO;
 import lk.ousl.initiators.pos.entity.OrderDetails;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface OrderDetailsDAO extends CrudDAO<OrderDetails, String > {
 }
